8d8cdec37df51ee6d85a26774b525b148dfb1ab0
Nathan Wolf <nathan@elmakers.com>
2026-08-23 13:30:09 +0200

    Update import script to handle one big file and automatically create categories

--- scripts/import.php
+++ scripts/import.php
@@ -30,6 +30,12 @@ try {
     $toPhrases = $db->getLanguagePhrases($toLanguage);
     $toPhrases = $db->index($toPhrases, 'text');
 
+    $categories = $db->getCategories();
+    $fromCategoryNames = $db->getCategoryNames($fromLanguage);
+    $fromCategoryNames = $db->index($fromCategoryNames, 'name');
+    $toCategoryNames = $db->getCategoryNames($toLanguage);
+    $toCategoryNames = $db->index($toCategoryNames, 'name');
+
     echo "Loading $filename\n";
 
     $file = fopen($filename, "r");
@@ -41,21 +47,39 @@ try {
         throw new Exception("Failed to find header row in file $filename");
     }
     $headers = array_flip($headers);
-    if (!isset($headers['from']) || !isset($headers['to']) || !isset($headers['category'])) {
+    if (!isset($headers['English']) || !isset($headers['Svenska']) || !isset($headers['Category English']) || !isset($headers['Category Svenska'])) {
         throw new Exception("Failed to find required column in file $filename");
     }
-    $fromIndex = $headers['from'];
-    $toIndex = $headers['to'];
-    $categoryIndex = $headers['category'];
+    $fromIndex = $headers['English'];
+    $toIndex = $headers['Svenska'];
+    $fromCategoryIndex = $headers['Category English'];
+    $toCategoryIndex = $headers['Category Svenska'];
     $phrases = array();
     while (($row = fgetcsv($file, null, ',', '"', '\\')) !== FALSE) {
         $fromText = sanitize($row[$fromIndex]);
         $toText = sanitize($row[$toIndex]);
-        $categoryId = $row[$categoryIndex];
+        $fromCategory = $row[$fromCategoryIndex];
+        $toCategory = $row[$toCategoryIndex];
 
         if (isset($fromPhrases[$fromText]) || isset($toPhrases[$toText])) {
-            echo "SKIPPED: $fromText\n";
+            echo "Skipped: $fromText\n";
             continue;
+        }
+
+        if (isset($fromCategoryNames[$fromCategory])) {
+            $categoryId = $fromCategoryNames[$fromCategory]['category_id'];
+            if (!isset($toCategoryNames[$toCategory]) || $toCategoryNames[$toCategory]['category_id'] != $categoryId) {
+                throw new Exception("Category mismatch for $fromCategory ($categoryId) and $toCategory");
+            }
+        } else {
+            echo "Adding new category: $fromCategory\n";
+            $categoryId = guidv4();
+            $db->insert('category', array('id' => $categoryId));
+            $db->insert('category_name', array('category_id' => $categoryId, 'language_id' => $fromLanguage, 'name' => $fromCategory));
+            $db->insert('category_name', array('category_id' => $categoryId, 'language_id' => $toLanguage, 'name' => $toCategory));
+            $fromCategoryNames[$fromCategory] = array('category_id' => $categoryId);
+            $toCategoryNames[$toCategory] = array('category_id' => $categoryId);
+            $categories[$categoryId] = array('id' => $categoryId);
         }
 
         echo "Adding: $fromText\n";
@@ -85,5 +109,20 @@ try {
 }
 
 function sanitize($text) {
-    return str_replace('’', "'", $text);
+    return trim(str_replace('’', "'", $text));
+}
+
+function guidv4($data = null) {
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
